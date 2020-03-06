09a18c9e9f40965320286f7c6eb959ba8009d454
Adrian Gunia <adrian.gunia@tieto.com>
2020-03-06 12:56:38 +0100

    Init heroku

---
+++ build/precache-manifest.ce5aeb2cfbd02d9648db13d8f1958677.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6821b1137c0b3e473a79ce35143b6549",
+    "url": "/index.html"
+  },
+  {
+    "revision": "94ac1304d0c0c57e469d",
+    "url": "/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "6574f9a4cd5dd1dbdc87",
+    "url": "/static/js/2.149ba1d4.chunk.js"
+  },
+  {
+    "revision": "37b5fb9fa73e0688add42527cf5813eb",
+    "url": "/static/js/2.149ba1d4.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "94ac1304d0c0c57e469d",
+    "url": "/static/js/main.35a20971.chunk.js"
+  },
+  {
+    "revision": "33fc1e030c2e76f7491b",
+    "url": "/static/js/runtime-main.8b0fcaf5.js"
+  }
+]);
